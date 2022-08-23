183ec58ff1e529708377ea5e7f1acbd01386cf64
none <none>
2022-08-23 20:41:43 +0200

    automatically create tables if they don't exist

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import models
 import sockets
 
 app = flask.Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.add_url_rule("/", view_func=views.render_home)
 app.add_url_rule("/<string:room_name>", view_func=views.render_chat)
@@ -15,5 +15,6 @@ sockets.socketio.init_app(app)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # For deploying on Heroku
+    with app.app_context(): models.db.create_all()  # create tables if they don't exist
+    port = int(os.environ.get("PORT", 5000))  # port 5000 for deploying on Heroku
     sockets.socketio.run(app, host="0.0.0.0", port=port)
