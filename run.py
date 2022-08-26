648376004e76ae40b6cac01b4a7f11b3d36961c2
none <none>
2022-08-26 17:58:38 +0200

    changed formatting

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import os
 import web_chat
 
 if __name__ == "__main__":
-	HOST = "0.0.0.0"
-	PORT = 5000
-	print(f"Running on {HOST}:{PORT}")
-	# Using an embedded server with eventlet 
-	# https://flask-socketio.readthedocs.io/en/latest/deployment.html
-	web_chat.sockets.socketio.run(web_chat.app, host=HOST, port=PORT)
+    HOST = "0.0.0.0"
+    PORT = 5000
+    print(f"Running on {HOST}:{PORT}")
+    # Using an embedded server with eventlet
+    # https://flask-socketio.readthedocs.io/en/latest/deployment.html
+    web_chat.sockets.socketio.run(web_chat.app, host=HOST, port=PORT)
